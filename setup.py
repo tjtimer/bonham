59644d159c80e3ce57685e7e2d4dad9d66e19788
tjtimer <tjtimer@gmail.com>
2017-02-17 16:54:43 +0100

    added rsa directory, updated .gitignore

--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup
 
-setup(name='bonham',
-      version='0.0.1',
-      description='aiohttp based web server',
-      url='https://github.com/tjtimer/bonham',
-      author='tjtimer',
-      author_email='tjtimer@gmail.com',
-      license='MIT',
-      packages=['bonham'],
-      zip_safe=False,
-      install_requires=['aiohttp',
-                        'cchardet',
-                        'cryptography',
-                        'uvloop',
-                        'sqlalchemy',
-                        'asyncpg', 'jinja2', 'aiohttp_jinja2', 'sqlamp', 'PyJWT', 'arrow',
-                        'requests', 'hypothesis', 'pytest'])
+setup(
+        name='bonham',
+        version='0.0.1',
+        description='aiohttp based web server',
+        url='https://github.com/tjtimer/bonham',
+        author='tjtimer',
+        author_email='tjtimer@gmail.com',
+        license='MIT',
+        packages=['bonham'],
+        zip_safe=False,
+        install_requires=[
+            'aiohttp',
+            'cchardet',
+            'cryptography',
+            'uvloop',
+            'sqlalchemy',
+            'asyncpg',
+            'jinja2',
+            'aiohttp_jinja2',
+            'sqlamp',
+            'PyJWT',
+            'arrow',
+            'requests',
+            'hypothesis',
+            'pytest'
+        ]
+)

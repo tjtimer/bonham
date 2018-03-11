910e22c15ca1e7317ec0337589b47b5dcfdfc9c9
tjtimer <tjtimer@gmail.com>
2018-03-11 15:12:45 +0100

    removed CLI bu folder

--- bonham/CLI/bu/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-JS_DIR = os.path.join(os.path.dirname(ROOT_DIR), 'bonham_ui', 'src')
-FUNCTION_CALL = "python app.py 9090"
--- bonham/CLI/bu/templates/strings.py
+++
@@ -1,9 +0,0 @@
-"""
-    Author: Tim "tjtimer "Jedro
-"""
-
-async def init_string(config):
-    yield f"# {config['app_dir']} / {config['app_name']}"
-    yield ""
-    yield "__all__ = []"
-

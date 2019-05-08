648ea2414b8afee1e922ecfdb5d8fb5566464b4a
tjtimer <tjtimer@gmail.com>
2019-05-08 21:38:36 +0200

    added click

--- setup.py
+++ setup.py
@@ -3,18 +3,31 @@ from setuptools import find_packages, setup
 setup(
     name='bonham',
     version='0.0.1.dev',
-    description='python aiohttp based web app',
+    description='CLI tool for web app projects with python and Vue.js.',
     url='https://github.com/tjtimer/bonham',
     author='Tim "tjtimer" Jedro',
-    author_email='tjtimer@gmail.com', license='MIT', packages=find_packages('bonham'),
-    package_dir={'bonham': 'bonham'},
+    author_email='tjtimer@gmail.com',
+    license='MIT',
+    packages=find_packages(),
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            # 'bonham-run=bonham.app:run',
-            # 'bonham-init=bonham.CLI.init_project:run', 'bonham-create=bonham.CLI.create:run',
+            'bonham=bonham.cli:cli'
         ]
     },
     zip_safe=False,
-    install_requires=['aiohttp', 'aiofiles', 'passlib', 'PyYaml', 'PyJWT', 'cryptography']
+    install_requires=[
+        'click',
+        'sanic',
+        'aiofiles',
+        'passlib',
+        'PyYaml',
+        'PyJWT',
+        'cryptography',
+        'pytest',
+        'pytest-sanic',
+        'pytest-aiohttp',
+        'jinja2-sanic',
+        'hypothesis'
+    ]
 )

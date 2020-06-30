3a87064dc659c2383d2102a9e518e8de6593aacf
tjtimer <tjtimer@gmail.com>
2020-06-30 03:01:50 +0200

    kinda initial commit

---
+++ __version__.py
@@ -0,0 +1,9 @@
+"""
+bonham
+
+__version__
+
+Author: Tim "tjtimer" Jedro
+Email: tjtimer@gmail.com
+created: 30.06.20
+"""
---
+++ bonham/core/config.py
@@ -0,0 +1,9 @@
+"""
+bonham
+
+config
+
+Author: Tim "tjtimer" Jedro
+Email: tjtimer@gmail.com
+created: 30.06.20
+"""
